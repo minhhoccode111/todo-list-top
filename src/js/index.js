47075da16bb2f24a5358b506d117e85ad4944f28
minhhoccode111 <minhhoccode111@gmail.com>
2023-06-22 23:40:37 +0700

    UI add Todo input dialog

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // import css
 import "./../css/responsive.css";
 import "./../css/animation.css";
+import "./../css/dialogs.css";
 import "./../css/styles.css";
 import "./../css/header.css";
 import "./../css/footer.css";
@@ -13,3 +14,16 @@ import "animate.css";
 
 //import library
 import * as fns from "date-fns";
+
+const about = document.getElementById("of__about");
+const aboutBtn = document.getElementById("header__about");
+const aboutClose = document.getElementById("about__close");
+
+aboutBtn.addEventListener("click", () => {
+  about.show();
+  // about.style.display = "flex";
+});
+aboutClose.addEventListener("click", () => {
+  about.close();
+  about.style.display = "none";
+});
