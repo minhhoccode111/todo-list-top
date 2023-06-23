0270ae5c8f9553c1af9e9974f2418d5ca01ce79b
minhhoccode111 <minhhoccode111@gmail.com>
2023-06-23 10:24:08 +0700

    Structured Project's file system

--- src/js/index.js
+++ src/js/index.js
@@ -11,19 +11,22 @@ import "./../css/main.css";
 import "animate.css";
 
 //import javaScript
+import "./full.js";
+import "./form.js";
 
 //import library
 import * as fns from "date-fns";
 
-const about = document.getElementById("of__about");
-const aboutBtn = document.getElementById("header__about");
-const aboutClose = document.getElementById("about__close");
+// #################### ABOUT BUTTON CLICK ####################
+// const about = document.getElementById("of__about");
+// const aboutBtn = document.getElementById("header__about");
+// const aboutClose = document.getElementById("about__close");
 
-aboutBtn.addEventListener("click", () => {
-  about.show();
-  // about.style.display = "flex";
-});
-aboutClose.addEventListener("click", () => {
-  about.close();
-  about.style.display = "none";
-});
+// aboutBtn.addEventListener("click", () => {
+//   about.show();
+//   // about.style.display = "flex";
+// });
+// aboutClose.addEventListener("click", () => {
+//   about.close();
+//   about.style.display = "none";
+// });
