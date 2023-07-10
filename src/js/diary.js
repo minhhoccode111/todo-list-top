d805fec2ac936209f6840d1b68707a32a4ceb4f2
minhhoccode111 <minhhoccode111@gmail.com>
2023-07-10 23:26:05 +0700

    New Note feature

--- src/js/diary.js
+++ src/js/diary.js
@@ -1,30 +1,27 @@
 //this is diary.js
 
-import { Diary } from "./create.js";
-import * as database from "./database.js";
-import * as Prototype from "./prototype.js";
-import { isYesterday, isBefore, startOfToday } from "date-fns";
-import * as Data from "./data.js";
+import { Diary } from './create.js';
+import * as database from './database.js';
+import * as Prototype from './prototype.js';
+import { isBefore, startOfToday } from 'date-fns';
+import * as Data from './data.js';
 
-let obj = Diary(
-  "I am not productive, I feel sad about that",
-  "I will push harder tomorrow! Learn and do new project on The Odin Project and become a Backend Engineer. I Can do it!"
-);
+let obj = Diary('I am not productive, I feel sad about that', 'I will push harder tomorrow! Learn and do new project on The Odin Project and become a Backend Engineer. I Can do it!');
 
-const set = () => database.set(obj, "diary");
+const set = () => database.set(obj, 'diary');
 
 export const get = () => obj;
 
 export const load = () => {
-  if (database.check("diary")) {
-    obj = Object.assign(Object.create(Prototype.diary), database.get("diary"));
+  if (database.check('diary')) {
+    obj = Object.assign(Object.create(Prototype.diary), database.get('diary'));
   }
   //is the created date of the diary is yesterday, then add it to 'diary' project in data, then update data to database, then create new Diary for today, then update that to 'diary' in database
   const today = startOfToday();
   if (isBefore(new Date(obj.createdDate), today)) {
-    Data.add(obj, "diary");
+    Data.add(obj, 'diary');
     Data.set();
-    obj = Diary("write your thoughts here", "write your thoughts here");
+    obj = Diary('write your thoughts here', 'write your thoughts here');
     set();
   }
 };
@@ -40,53 +37,53 @@ export const night = (v) => {
 };
 
 export const typeInput = (obj) => {
-  const container = document.createElement("div");
-  container.id = "section__diary__inputs__ctn";
+  const container = document.createElement('div');
+  container.id = 'section__diary__inputs__ctn';
 
-  const lastModified = document.createElement("em");
-  lastModified.id = "diary__last__modified";
+  const lastModified = document.createElement('em');
+  lastModified.id = 'diary__last__modified';
   lastModified.textContent = `Last modified: ${obj.lastModified}`;
   container.appendChild(lastModified);
 
-  const formDay = document.createElement("div");
-  formDay.id = "diary__form__day";
+  const formDay = document.createElement('div');
+  formDay.id = 'diary__form__day';
   container.appendChild(formDay);
 
-  const dayParagraph = document.createElement("p");
-  dayParagraph.textContent = "Your thoughts all day long.";
+  const dayParagraph = document.createElement('p');
+  dayParagraph.textContent = 'Your thoughts all day long.';
   formDay.appendChild(dayParagraph);
 
-  const dayHr = document.createElement("hr");
+  const dayHr = document.createElement('hr');
   formDay.appendChild(dayHr);
 
-  const dayInput = document.createElement("div");
-  dayInput.id = "diary__input__day";
+  const dayInput = document.createElement('div');
+  dayInput.id = 'diary__input__day';
   dayInput.contentEditable = true;
   dayInput.spellcheck = false;
   dayInput.textContent = obj.day;
-  dayInput.addEventListener("input", (event) => {
+  dayInput.addEventListener('input', (event) => {
     obj.setLastModified();
     day(event.target.textContent);
   });
   formDay.appendChild(dayInput);
 
-  const formNight = document.createElement("div");
-  formNight.id = "diary__form__night";
+  const formNight = document.createElement('div');
+  formNight.id = 'diary__form__night';
   container.appendChild(formNight);
 
-  const nightParagraph = document.createElement("p");
-  nightParagraph.textContent = "Your thoughts before bed.";
+  const nightParagraph = document.createElement('p');
+  nightParagraph.textContent = 'Your thoughts before bed.';
   formNight.appendChild(nightParagraph);
 
-  const nightHr = document.createElement("hr");
+  const nightHr = document.createElement('hr');
   formNight.appendChild(nightHr);
 
-  const nightInput = document.createElement("div");
-  nightInput.id = "diary__input__night";
+  const nightInput = document.createElement('div');
+  nightInput.id = 'diary__input__night';
   nightInput.contentEditable = true;
   nightInput.spellcheck = false;
   nightInput.textContent = obj.night;
-  nightInput.addEventListener("input", (event) => {
+  nightInput.addEventListener('input', (event) => {
     obj.setLastModified();
     night(event.target.textContent);
   });
--- src/js/index.js
+++ src/js/index.js
@@ -48,4 +48,4 @@ export function resetApp() {
   database.set(null, 'dairy'); //typos
   database.set(null, 'date'); //typos
 }
-resetApp();
+// resetApp();
