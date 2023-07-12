5b374e462d7f7a156ce3d8c24a0b16d06315fcab
minhhoccode111 <minhhoccode111@gmail.com>
2023-07-12 17:38:00 +0700

    noteElement edited

--- src/js/dialogs.js
+++ src/js/dialogs.js
@@ -1,61 +1,45 @@
 //this is form.js
 
-import * as Create from "./create.js";
-import * as Current from "./current.js";
-import * as Display from "./display.js";
-import * as Data from "./data.js";
+import Data from './data.js';
+import Current from './current.js';
+import * as Create from './create.js';
+import * as Display from './display.js';
 
 export function listenForCreate(type) {
   const dialog = document.getElementById(`of__${type}`);
   const dueDateInput = document.getElementById(`dueDate__of__${type}`);
-  const inputs = document.querySelectorAll(
-    `input[name="hasDueDate__of__${type}"]`
-  );
+  const inputs = document.querySelectorAll(`input[name="hasDueDate__of__${type}"]`);
   inputs.forEach((input) => {
-    input.addEventListener("change", function () {
-      if (this.value === "yes") {
+    input.addEventListener('change', function () {
+      if (this.value === 'yes') {
         dueDateInput.disabled = false;
-        dueDateInput.setAttribute("required", "required");
+        dueDateInput.setAttribute('required', 'required');
       } else {
         dueDateInput.disabled = true;
-        dueDateInput.removeAttribute("required");
-        dueDateInput.value = "";
+        dueDateInput.removeAttribute('required');
+        dueDateInput.value = '';
       }
     });
   });
   const form = document.getElementById(`form__of__${type}`);
-  form.addEventListener("submit", function (event) {
+  form.addEventListener('submit', function (event) {
     event.preventDefault();
     // Retrieve form data
     let obj;
     const title = document.getElementById(`title__of__${type}`).value;
     const detail = document.getElementById(`detail__of__${type}`).value;
     const dueDate = document.getElementById(`dueDate__of__${type}`).value;
-    const hasDueDate =
-      document.querySelector(`input[name="hasDueDate__of__${type}"]:checked`)
-        .value === "yes";
+    const hasDueDate = document.querySelector(`input[name="hasDueDate__of__${type}"]:checked`).value === 'yes';
     const project = Current.get();
-    if (type === "todo") {
-      const priority = document.querySelector(
-        'input[name="priority__of__todo"]:checked'
-      ).value;
-      const isDone =
-        document.querySelector('input[name="isDone__of__todo"]:checked')
-          .value === "true";
-      obj = Create.Todo(
-        title,
-        detail,
-        dueDate,
-        hasDueDate,
-        priority,
-        isDone,
-        project
-      );
+    if (type === 'todo') {
+      const priority = document.querySelector('input[name="priority__of__todo"]:checked').value;
+      const isDone = document.querySelector('input[name="isDone__of__todo"]:checked').value === 'true';
+      obj = Create.Todo(title, detail, dueDate, hasDueDate, priority, isDone, project);
       Data.add(obj, project); //add to Data
-    } else if (type === "note") {
+    } else if (type === 'note') {
       obj = Create.Note(title, detail, dueDate, hasDueDate);
       Data.add(obj, project); //add to Data
-    } else if (type === "project") {
+    } else if (type === 'project') {
       obj = Create.Project(title, detail, dueDate, hasDueDate);
       Data.projects.add(title); //
       Data.add(obj, project); //add to Data
@@ -72,11 +56,9 @@ export function listenForCreate(type) {
   });
 
   //Close when click cancel
-  document
-    .querySelector(`input#cancel__of__${type}[value="Cancel"][type="button"]`)
-    .addEventListener("click", () => {
-      dialog.close();
-      dueDateInput.disabled = true;
-      form.reset();
-    });
+  document.querySelector(`input#cancel__of__${type}[value="Cancel"][type="button"]`).addEventListener('click', () => {
+    dialog.close();
+    dueDateInput.disabled = true;
+    form.reset();
+  });
 }
--- src/js/noteElement.js
+++ src/js/noteElement.js
@@ -1,52 +1,52 @@
 //this is noteElement.js
-import { displayInfo } from "./info.js";
-import * as Data from "./data.js";
-import * as Display from "./display.js";
+import { displayInfo } from './info.js';
+import Data from './data.js';
+import * as Display from './display.js';
 
 export function note(obj) {
   const { id, title, lastModified, detail } = obj;
 
-  const divNoteItem = document.createElement("div");
-  divNoteItem.className = "note__item";
-  divNoteItem.setAttribute("data-id", id);
+  const divNoteItem = document.createElement('div');
+  divNoteItem.className = 'note__item';
+  divNoteItem.setAttribute('data-id', id);
 
-  const divNoteHeader = document.createElement("div");
-  divNoteHeader.className = "note__item__header";
+  const divNoteHeader = document.createElement('div');
+  divNoteHeader.className = 'note__item__header';
 
-  const divNoteTitle = document.createElement("div");
-  divNoteTitle.className = "note__item__title";
+  const divNoteTitle = document.createElement('div');
+  divNoteTitle.className = 'note__item__title';
   divNoteTitle.contentEditable = true;
   divNoteTitle.spellcheck = false;
   divNoteTitle.textContent = title;
-  divNoteTitle.addEventListener("input", (e) => {
-    noteEditedInputs(obj, "title", e.target.textContent);
+  divNoteTitle.addEventListener('input', (e) => {
+    noteEditedInputs(obj, 'title', e.target.textContent);
   });
 
-  const emLastModified = document.createElement("em");
+  const emLastModified = document.createElement('em');
   emLastModified.textContent = lastModified;
 
-  const buttonInfo = document.createElement("button");
-  buttonInfo.className = "note__item__info info";
-  buttonInfo.textContent = "?";
-  buttonInfo.addEventListener("click", () => {
+  const buttonInfo = document.createElement('button');
+  buttonInfo.className = 'note__item__info info';
+  buttonInfo.textContent = '?';
+  buttonInfo.addEventListener('click', () => {
     displayInfo(obj);
   });
 
-  const buttonDel = document.createElement("button");
-  buttonDel.className = "note__item__del del";
-  buttonDel.textContent = "X";
-  buttonDel.addEventListener("click", () => {
+  const buttonDel = document.createElement('button');
+  buttonDel.className = 'note__item__del del';
+  buttonDel.textContent = 'X';
+  buttonDel.addEventListener('click', () => {
     divNoteItem.remove();
     noteClickedDelete(obj);
   });
 
-  const divNoteDetail = document.createElement("div");
-  divNoteDetail.className = "note__item__detail";
+  const divNoteDetail = document.createElement('div');
+  divNoteDetail.className = 'note__item__detail';
   divNoteDetail.contentEditable = true;
   divNoteDetail.spellcheck = false;
   divNoteDetail.textContent = detail;
-  divNoteDetail.addEventListener("input", (e) => {
-    noteEditedInputs(obj, "detail", e.target.textContent);
+  divNoteDetail.addEventListener('input', (e) => {
+    noteEditedInputs(obj, 'detail', e.target.textContent);
   });
 
   divNoteItem.appendChild(divNoteHeader);
@@ -66,7 +66,6 @@ function noteEditedInputs(obj, property, value) {
 }
 
 function noteClickedDelete(obj) {
-  const index = Data.projects.get("note").indexOf(obj);
-  Data.del("note", index);
+  Data.items.del(obj, 'note');
   Display.updateSpan();
 }
