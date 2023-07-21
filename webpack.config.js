5893e86f4bfdcfb553fed540f3499b917ffc2a93
minhhoccode111 <minhhoccode111@gmail.com>
2023-07-21 19:14:02 +0700

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,48 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/js/index.js",
+  entry: './src/js/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  mode: "development",
+  mode: 'development',
   devServer: {
-    static: "./dist",
+    static: './dist',
     port: 8080,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
       {
         test: /\.ico$/,
         use: {
-          loader: "file-loader",
+          loader: 'file-loader',
           options: {
-            name: "[name].[ext]",
-            outputPath: "images/", // or the appropriate output directory
+            name: '[name].[ext]',
+            outputPath: 'images/', // or the appropriate output directory
+          },
+        },
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+            plugins: ['@babel/plugin-proposal-class-properties'],
           },
         },
       },
